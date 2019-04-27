50f1a32891f81c8efe543c73b9dd537492f30894
Suat <git@su.at>
2019-04-28 01:58:19 +0200

    admin user id config

--- config.inc.example.php
+++ config.inc.example.php
@@ -5,3 +5,5 @@ define('BOT_NAME', 'FirePantherBot');
 define('BOT_TOKEN', '');
 define('REPORT_ERRORS', true); // for debugging
 
+// authorization (the telegram user id with admin permission)
+define('ADMIN_USER_ID', '');
