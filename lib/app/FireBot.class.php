d248a30fddffb2f35eb5e9007daeb6a6b2dd7344
Suat <git@su.at>
2019-04-27 02:48:40 +0200

    telegramRequest static object variable

--- lib/app/FireBot.class.php
+++ lib/app/FireBot.class.php
@@ -4,7 +4,7 @@ namespace app;
 use \telegram\Request;
 
 class FireBot {
-	private $telegramRequest = false;
+	private static $telegramRequest = false;
 	
 	function __construct(array $request = []) {
 		if ($request) $this->parseRequest($request);
@@ -12,16 +12,21 @@ class FireBot {
 	
 	// parses the webhook request
 	public function parseRequest(array $request) {
-		$this->telegramRequest = new Request($request);
+		self::$telegramRequest = new Request($request);
+	}
+	
+	// get the webhook request
+	public static final function getRequest() {
+		return self::$telegramRequest;
 	}
 	
 	// creates an object for the requested action
 	public function exec() {
-		if ($this->telegramRequest !== false) {
-			$className = ucfirst($this->telegramRequest->getPart(0));
+		if (self::$telegramRequest !== false) {
+			$className = ucfirst(self::$telegramRequest->getPart(0));
 			if (preg_match('~^\w+$~', $className) && file_exists(MAIN_DIR."/lib/action/$className.class.php")) {
 				$classNamespace = "\\action\\$className";
-				new $classNamespace($this->telegramRequest);
+				new $classNamespace(self::$telegramRequest);
 			}
 		}
 	}
