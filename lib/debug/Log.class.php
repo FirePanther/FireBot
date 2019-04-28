0f179bed253b166e520306462d31cfaef85baa8a
Suat <git@su.at>
2019-04-29 00:18:05 +0200

    log arrays

--- lib/debug/Log.class.php
+++ lib/debug/Log.class.php
@@ -3,10 +3,12 @@ namespace debug;
 
 class Log {
 	// quickly log via telegram (for debugging)
-	public static function message($text) {
+	public static function message($text, $icon = false) {
 		if (defined('DEBUG') && DEBUG) {
+			$text = is_array($text) ? print_r($text, 1) : $text;
 			\util\File::write(MAIN_DIR.'/log/last-manual-log', $text);
-			$response = new \telegram\Response($text);
+			$response = new \telegram\Response(($icon ? $icon.' ' : '').$text);
+			$response->setDebug(true);
 			$response->emit(defined('ADMIN_USER_ID') ? ADMIN_USER_ID : null);
 		}
 	}
@@ -16,18 +18,18 @@ class Log {
 	}
 	
 	public static function error($text) {
-		self::message('❌ '.$text);
+		self::message($text, '❌');
 	}
 	
 	public static function warning($text) {
-		self::message('⚠️ '.$text);
+		self::message($text, '⚠️');
 	}
 	
 	public static function success($text) {
-		self::message('✅ '.$text);
+		self::message($text, '✅');
 	}
 	
 	public static function info($text) {
-		self::message('ℹ️ '.$text);
+		self::message($text, 'ℹ️');
 	}
 }
